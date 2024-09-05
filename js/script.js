9f9ffc90319cdef08b3463b00ef2b20d8e1ab4ef
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-05 13:20:27 +0200

    html + js progress

--- js/script.js
+++ js/script.js
@@ -21,8 +21,20 @@ createApp({
     return {
       todoList: [
         {
-          text: "",
-          done: "",
+          text: "Fare i compiti",
+          done: false,
+        },
+        {
+          text: "Fare la spesa",
+          done: true,
+        },
+        {
+          text: "Pagare l'assicurazione",
+          done: false,
+        },
+        {
+          text: "Andare in palestra",
+          done: true,
         },
       ],
     };
